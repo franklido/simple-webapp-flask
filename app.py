136a92194ecbc5d72f9aacbcc551939113bbb9f2
franklido <107265505+franklido@users.noreply.github.com>
2022-06-13 10:26:12 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import flask
+app = flask(__name__)
+@app.route("/")
+def main():
+	return "Welcome!"
+
+@app.route("/mela")
+def mela():
+	return "Le pomme est rouge"
+
+if __name__ == "__main__":
+	app.run()
